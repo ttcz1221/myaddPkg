56f0158d782abed5e1ba56412e41806df4edd6d8
ttcz1221 <1812205494@qq.com>
2022-12-09 16:31:32 +0800

    v0.2

---
+++ R/add.R
@@ -0,0 +1,17 @@
+#' 加法
+#'
+#' @param x 参数x
+#' @param y 参数y
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' b = myadd(3,4)
+#' c = myadd(x=5,y=6)
+#' d = myadd(y=2,x=3)
+myadd <- function(x=1,y=2) {
+  res = x+y
+  return(res)
+
+}
